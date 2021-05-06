9f975c71108cdd79b6fbc780be4cfe2aff91d11c
nico <prananta@swift.ch>
2021-05-06 14:08:27 +0200

    test ssr

--- pages/day/index.js
+++ pages/day/index.js
@@ -1,3 +1,27 @@
-export default function DayPage() {
-  return <div>Hello Day</div>
+export default function DayPage({ facts }) {
+  return (
+    <div>
+      <ul>
+        {facts &&
+          facts.map((f, i) => {
+            return <li key={i}>{f.text}</li>;
+          })}
+      </ul>
+    </div>
+  );
+}
+
+export async function getServerSideProps(context) {
+  await new Promise((r) => {
+    setTimeout(() => {
+      r();
+    }, 5000);
+  });
+  const res = await fetch(`https://cat-fact.herokuapp.com/facts`);
+  const data = await res.json();
+  return {
+    props: {
+      facts: data,
+    }, // will be passed to the page component as props
+  };
 }
--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,24 @@
-import Link from 'next/link'
+import Link from "next/link";
+import { useState } from "react";
 
 export default function IndexPage() {
+  const [loading, setLoading] = useState(false);
   return (
     <div>
-      Hello World.{' '}
+      Hello World.{" "}
       <Link href="/about">
         <a>About</a>
       </Link>
+      <Link href="/day">
+        <a
+          onClick={() => {
+            setLoading(true);
+          }}
+        >
+          Cats
+        </a>
+      </Link>
+      {loading && <p>Loading...</p>}
     </div>
-  )
+  );
 }
